1bd270dd762426f636ff3715dd361f2969c0dbcb
Fadhilatur Rochman <aldhinya.aldhi@yahoo.com>
2018-05-21 20:14:42 +0700

    Create data.h

---
+++ Semester 2/Modul 1/Tugas Tambahan/Tambahan Dosen/data.h
@@ -0,0 +1,16 @@
+#ifndef DATA_H_INCLUDED
+#define DATA_H_INCLUDED
+
+struct
+{
+    char nama[50];
+    char kelas[10];
+    char npm[50];
+} dataMahasiswa[50];
+
+struct
+{
+    int quiz, tugas, ets, eas, akhir;
+} dataNilai[50];
+
+#endif // DATA_H_INCLUDED
