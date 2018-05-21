3ac2db16e1594afba3e567f942590788a6b63052
Fadhilatur Rochman <aldhinya.aldhi@yahoo.com>
2018-05-21 20:09:27 +0700

    Create header.h

---
+++ Semester 2/Modul 1/Tugas Tambahan/Tambahan 1/header.h
@@ -0,0 +1,16 @@
+#ifndef DATA_H_INCLUDED
+#define DATA_H_INCLUDED
+
+struct
+{
+    char nama[50];
+    char kelas[10];
+    char npm[50];
+} dataMahasiswa;
+
+struct
+{
+    int quiz, tugas, ets, eas, akhir;
+} dataNilai;
+
+#endif // DATA_H_INCLUDED
